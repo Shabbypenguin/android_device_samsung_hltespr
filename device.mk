d40303ef473fe118f78cfcf61b49d22e4bf17f3b
David Rouleau <Shabbypenguin@gmail.com>
2013-10-08 13:33:18 -0400

    Update to source built kernel

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/hltespr/overlay
 
 LOCAL_PATH := device/samsung/hltespr
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel.stock
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
